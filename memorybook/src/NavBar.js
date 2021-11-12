433ee990ad9490de9f11e6deafc783052c644886
Pumzow <32910165+Pumzow@users.noreply.github.com>
2021-11-12 09:07:22 +0200

    Application Initialization

---
+++ memorybook/src/NavBar.js
@@ -0,0 +1,24 @@
+
+
+function NavBar() {
+  return (
+      <header className="NavBar">
+        <h1 className="NavBar-Logo"> MemoryBook </h1>
+        <nav className="NavBar-Menu">
+            <ul>
+                <li>
+                    Memories
+                </li>
+                <li>
+                    Chat
+                </li>
+                <li>
+                    Profile
+                </li>
+            </ul>
+        </nav>
+      </header>
+  );
+}
+
+export default NavBar;
