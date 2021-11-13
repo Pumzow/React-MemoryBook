708827d38bfe1678edfa7daa21ffd8585fade303
Pumzow <32910165+Pumzow@users.noreply.github.com>
2021-11-13 16:09:32 +0200

    Implemented Page Router

--- memorybook/src/App.js
+++ memorybook/src/App.js
@@ -1,3 +1,5 @@
+import { useState, createElement } from 'react';
+
 import Header from './components/Header.js';
 import Latest from './components/Latest.js';
 import Book from './components/Book.js';
@@ -5,16 +7,25 @@ import Footer from './components/Footer';
 import './App.css';
 
 function App() {
-const routes = {
-  '/Latest': Latest,
-  '/Book': Book,
-};
+  const [page, setPage] = useState('/Latest')
+
+  const routes = {
+    '/Latest': <Latest />,
+    '/Book': <Book />,
+  };
+
+  const navigationChangeHandler = (path) =>{
+    setPage(path);
+  }
 
   return (
     <div className="App">
-      <Header />
-      <Latest />
-      <Book />
+      <Header navigationChangeHandler={navigationChangeHandler}/>
+
+      <main>
+        {routes[page] || <h2> 404 Not Found! </h2> }
+      </main>
+
       <Footer />
     </div>
   );
