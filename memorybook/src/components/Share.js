e02ee28c8c388983c44868fbe2cf65af24707ca4
Pumzow <32910165+Pumzow@users.noreply.github.com>
2021-11-20 03:59:20 +0200

    Share Page Image Preview Implemented

--- memorybook/src/components/Share.js
+++ memorybook/src/components/Share.js
@@ -8,16 +8,19 @@ const Book = () => {
         <h1> Share Memory </h1>
         <form id="Share-Form">
             <div className="container">
-              <label htmlFor="leg-title"> Title: </label>
+              <label htmlFor="leg-title"> Title </label><br/>
               <input id="Share-Form-Title" type="text" name="title" placeholder="Enter memory title..." /><br/>
 
-              <label htmlFor="category"> Description: </label>
-              <input id="Share-Form-Description" type="text" name="description" placeholder="Enter memory description..." /><br/>
+              <label htmlFor="category"> Description </label><br/>
+              <textarea id="Share-Form-Description" type="text" name="description" placeholder="Enter memory description..." /><br/>
 
-              <label htmlFor="game-img">Image URL:</label>
-              <input id="Share-Form-ImageURL" type="text" name="imageUrl" placeholder="Upload a photo..." /><br/>
+              <label htmlFor="game-img">Image URL </label><br/>
+              <input id="Share-Form-ImageURL" type="text" name="imageUrl" placeholder="Upload a photo..." onChange={ChangeMemoryPreviewImage}/><br/>
 
               <input className="btn submit" type="submit" value="Share" onClick={ShareMemory}/>
+            </div>
+            <div id="MemoryPreviewImage-parent">
+              <img id="MemoryPreviewImage" src="https://merriam-webster.com/assets/mw/images/gallery/gal-home-edpick-lg/empty-speech-bubble-7508-68642ecb0f0a19313dd31c16f67e67e1@1x.jpg" alt=""/>
             </div>
         </form>
     </section>
@@ -26,6 +29,22 @@ const Book = () => {
   
 };
 
+function ChangeMemoryPreviewImage(e){
+  e.preventDefault();
+
+  
+
+  const memoryPreviewImageRef = document.querySelector('#MemoryPreviewImage');
+  const imageUrlRef = document.querySelector('#Share-Form-ImageURL');
+  
+  memoryPreviewImageRef.src = imageUrlRef.value
+
+  if(imageUrlRef.value == "")
+  {
+    memoryPreviewImageRef.src = "https://merriam-webster.com/assets/mw/images/gallery/gal-home-edpick-lg/empty-speech-bubble-7508-68642ecb0f0a19313dd31c16f67e67e1@1x.jpg";
+  }
+
+}
 
 async function ShareMemory(e){
 
@@ -36,7 +55,7 @@ async function ShareMemory(e){
   const imageUrlRef = document.querySelector('#Share-Form-ImageURL');
 
   const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  
+
   const date = new Date();
   const sharedDate = date.getDate() + " " + monthNames[date.getMonth()] + " " + date.getFullYear();
 
